7d0ed5cabc2d458dd52c3208d710b22cd61c881f
Anildudy <anildudy@users.noreply.github.com>
2026-05-31 12:18:27 +0530

    Initial dotfiles

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -e
+
+DOTFILES_DIR="$HOME/.mydotfiles/com.ml4w.dotfiles.stable"
+
+echo ":: Installing packages..."
+sudo pacman -S --needed - < "$DOTFILES_DIR/pkglist.txt"
+yay -S --needed - < "$DOTFILES_DIR/pkglist-aur.txt"
+
+echo ":: Creating symlinks..."
+ln -sf "$DOTFILES_DIR/.bashrc" "$HOME/.bashrc"
+ln -sf "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
+ln -sf "$DOTFILES_DIR/.Xresources" "$HOME/.Xresources"
+ln -sf "$DOTFILES_DIR/.gtkrc-2.0" "$HOME/.gtkrc-2.0"
+
+ln -sf "$DOTFILES_DIR/.config/bashrc" "$HOME/.config/bashrc"
+ln -sf "$DOTFILES_DIR/.config/btop" "$HOME/.config/btop"
+ln -sf "$DOTFILES_DIR/.config/chromium-flags.conf" "$HOME/.config/chromium-flags.conf"
+ln -sf "$DOTFILES_DIR/.config/edge-flags.conf" "$HOME/.config/edge-flags.conf"
+ln -sf "$DOTFILES_DIR/.config/fastfetch" "$HOME/.config/fastfetch"
+ln -sf "$DOTFILES_DIR/.config/fish" "$HOME/.config/fish"
+ln -sf "$DOTFILES_DIR/.config/gtk-3.0" "$HOME/.config/gtk-3.0"
+ln -sf "$DOTFILES_DIR/.config/gtk-4.0" "$HOME/.config/gtk-4.0"
+ln -sf "$DOTFILES_DIR/.config/hypr" "$HOME/.config/hypr"
+ln -sf "$DOTFILES_DIR/.config/kitty" "$HOME/.config/kitty"
+ln -sf "$DOTFILES_DIR/.config/matugen" "$HOME/.config/matugen"
+ln -sf "$DOTFILES_DIR/.config/ml4w" "$HOME/.config/ml4w"
+ln -sf "$DOTFILES_DIR/.config/ml4w-dotfiles-settings" "$HOME/.config/ml4w-dotfiles-settings"
+ln -sf "$DOTFILES_DIR/.config/nwg-dock-hyprland" "$HOME/.config/nwg-dock-hyprland"
+ln -sf "$DOTFILES_DIR/.config/ohmyposh" "$HOME/.config/ohmyposh"
+ln -sf "$DOTFILES_DIR/.config/qt6ct" "$HOME/.config/qt6ct"
+ln -sf "$DOTFILES_DIR/.config/quickshell" "$HOME/.config/quickshell"
+ln -sf "$DOTFILES_DIR/.config/rofi" "$HOME/.config/rofi"
+ln -sf "$DOTFILES_DIR/.config/sidepad" "$HOME/.config/sidepad"
+ln -sf "$DOTFILES_DIR/.config/swaync" "$HOME/.config/swaync"
+ln -sf "$DOTFILES_DIR/.config/walker" "$HOME/.config/walker"
+ln -sf "$DOTFILES_DIR/.config/waybar" "$HOME/.config/waybar"
+ln -sf "$DOTFILES_DIR/.config/waypaper" "$HOME/.config/waypaper"
+ln -sf "$DOTFILES_DIR/.config/wlogout" "$HOME/.config/wlogout"
+ln -sf "$DOTFILES_DIR/.config/xsettingsd" "$HOME/.config/xsettingsd"
+ln -sf "$DOTFILES_DIR/.config/zshrc" "$HOME/.config/zshrc"
+
+echo ":: Done! Reboot to apply."
